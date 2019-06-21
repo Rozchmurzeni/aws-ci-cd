591d311e72bde9d4f6aac4fdfdba8cbace9d6a8d
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2019-06-21 20:58:09 +0200

    Complete CreateOffer function implementation; add serverless template

--- backend/LoanOfferer/Models/Requests/CreateOfferRequest.cs
+++ backend/LoanOfferer/Models/Requests/CreateOfferRequest.cs
@@ -1,10 +1,20 @@
 using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
 
 namespace LoanOfferer.Models.Requests
 {
     public class CreateOfferRequest : APIGatewayProxyRequest
     {
-        public string PeselNumber { get; set; }
-        public string EmailAddress { get; set; }
+        private CreateOfferBodyRequest _requestBody;
+        private CreateOfferBodyRequest RequestBody => _requestBody ?? (_requestBody = JsonConvert.DeserializeObject<CreateOfferBodyRequest>(Body));
+
+        public string PeselNumber => RequestBody.PeselNumber;
+        public string EmailAddress => RequestBody.EmailAddress;
+
+        private class CreateOfferBodyRequest
+        {
+            public string PeselNumber { get; set; }
+            public string EmailAddress { get; set; }
+        }
     }
 }
--- backend/LoanOfferer/Models/Responses/CreateOfferResponse.cs
+++ backend/LoanOfferer/Models/Responses/CreateOfferResponse.cs
@@ -1,10 +1,22 @@
 using System;
+using System.Net;
 using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
 
 namespace LoanOfferer.Models.Responses
 {
     public class CreateOfferResponse : APIGatewayProxyResponse
     {
-        public CreateOfferResponse(Guid id, int maxValue) {}
+        public CreateOfferResponse(Guid id, int maxLoanAmount)
+        {
+            StatusCode = (int) HttpStatusCode.Created;
+            Body = JsonConvert.SerializeObject(
+                new
+                {
+                    Id = id,
+                    MaxLoanAmount = maxLoanAmount
+                }
+            );
+        }
     }
 }
