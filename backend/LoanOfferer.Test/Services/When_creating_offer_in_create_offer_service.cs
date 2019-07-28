3e15b284f9d11bd7d4777dc7ed7c862acc11d48f
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2019-07-28 22:18:28 +0200

    Add tests for RequestLoanService

--- backend/LoanOfferer.Test/Services/When_creating_offer_in_create_offer_service.cs
+++ backend/LoanOfferer.Test/Services/When_creating_offer_in_create_offer_service.cs
@@ -1,6 +1,10 @@
 using System.Threading.Tasks;
 using FluentAssertions;
-using LoanOfferer.Test.Helpers.Builders;
+using LoanOfferer.Domain.Entities;
+using LoanOfferer.Domain.Factories;
+using LoanOfferer.Domain.Repositories;
+using LoanOfferer.Domain.Services;
+using LoanOfferer.Services;
 using Moq;
 using Xunit;
 
@@ -14,16 +18,14 @@ namespace LoanOfferer.Test.Services
             // Given
             const string peselNumber = "testPeselNumber";
             const string emailAddress = "testEmailAddress";
-            var serviceBuilder = new CreateOfferServiceBuilder();
-            var service = serviceBuilder
-                         .WithLoanOfferFactoryReturningLoanOffer(peselNumber, emailAddress)
-                         .Build();
+            var factoryMock = CreateConfiguredLoanOfferFactoryMock(peselNumber, emailAddress);
+            var service = CreateOfferService(factoryMock);
 
             // When
             await service.CreateOfferAsync(peselNumber, emailAddress);
 
             // Then
-            serviceBuilder.LoanOfferFactoryMock.Verify(mock => mock.Create(peselNumber, emailAddress), Times.Once);
+            factoryMock.Verify(mock => mock.Create(peselNumber, emailAddress), Times.Once);
         }
 
         [Fact]
@@ -32,16 +34,16 @@ namespace LoanOfferer.Test.Services
             // Given
             const string peselNumber = "testPeselNumber";
             const string emailAddress = "testEmailAddress";
-            var serviceBuilder = new CreateOfferServiceBuilder();
-            var service = serviceBuilder
-                         .WithLoanOfferFactoryReturningLoanOffer(peselNumber, emailAddress)
-                         .Build();
+            var loanOfferMock = new Mock<ILoanOffer>();
+            var scoringServiceMock = new Mock<IScoringService>();
+            var factoryMock = CreateConfiguredLoanOfferFactoryMock(peselNumber, emailAddress, loanOfferMock);
+            var service = CreateOfferService(factoryMock, scoringServiceMock);
 
             // When
             await service.CreateOfferAsync(peselNumber, emailAddress);
 
             // Then
-            serviceBuilder.LoanOfferMock.Verify(mock => mock.CalculateOffer(serviceBuilder.ScoringServiceMock.Object), Times.Once);
+            loanOfferMock.Verify(mock => mock.CalculateOffer(scoringServiceMock.Object), Times.Once);
         }
 
         [Fact]
@@ -50,16 +52,16 @@ namespace LoanOfferer.Test.Services
             // Given
             const string peselNumber = "testPeselNumber";
             const string emailAddress = "testEmailAddress";
-            var serviceBuilder = new CreateOfferServiceBuilder();
-            var service = serviceBuilder
-                         .WithLoanOfferFactoryReturningLoanOffer(peselNumber, emailAddress)
-                         .Build();
+            var loanOfferMock = new Mock<ILoanOffer>();
+            var repositoryMock = new Mock<ILoanOfferRepository>();
+            var factoryMock = CreateConfiguredLoanOfferFactoryMock(peselNumber, emailAddress, loanOfferMock);
+            var service = CreateOfferService(factoryMock, loanOfferRepositoryMock: repositoryMock);
 
             // When
             await service.CreateOfferAsync(peselNumber, emailAddress);
 
             // Then
-            serviceBuilder.LoanOfferRepositoryMock.Verify(mock => mock.AddAsync(serviceBuilder.LoanOfferMock.Object), Times.Once);
+            repositoryMock.Verify(mock => mock.AddAsync(loanOfferMock.Object), Times.Once);
         }
 
         [Fact]
@@ -68,16 +70,38 @@ namespace LoanOfferer.Test.Services
             // Given
             const string peselNumber = "testPeselNumber";
             const string emailAddress = "testEmailAddress";
-            var serviceBuilder = new CreateOfferServiceBuilder();
-            var service = serviceBuilder
-                         .WithLoanOfferFactoryReturningLoanOffer(peselNumber, emailAddress)
-                         .Build();
+            var loanOfferMock = new Mock<ILoanOffer>();
+            var factoryMock = CreateConfiguredLoanOfferFactoryMock(peselNumber, emailAddress, loanOfferMock);
+            var service = CreateOfferService(factoryMock);
 
             // When
             var result = await service.CreateOfferAsync(peselNumber, emailAddress);
 
             // Then
-            result.Should().Be(serviceBuilder.LoanOfferMock.Object);
+            result.Should().Be(loanOfferMock.Object);
         }
+
+        private static Mock<ILoanOfferFactory> CreateConfiguredLoanOfferFactoryMock(
+            string peselNumber,
+            string emailAddress,
+            IMock<ILoanOffer> loanOfferMock = null
+        )
+        {
+            var mock = new Mock<ILoanOfferFactory>();
+            mock.Setup(m => m.Create(peselNumber, emailAddress))
+                .Returns(loanOfferMock?.Object ?? new Mock<ILoanOffer>().Object);
+            return mock;
+        }
+
+        private static CreateOfferService CreateOfferService(
+            IMock<ILoanOfferFactory> loanOfferFactoryMock = null,
+            IMock<IScoringService> scoringServiceMock = null,
+            IMock<ILoanOfferRepository> loanOfferRepositoryMock = null
+        )
+            => new CreateOfferService(
+                loanOfferFactoryMock?.Object ?? new Mock<ILoanOfferFactory>().Object,
+                loanOfferRepositoryMock?.Object ?? new Mock<ILoanOfferRepository>().Object,
+                scoringServiceMock?.Object ?? new Mock<IScoringService>().Object
+            );
     }
 }
--- backend/LoanOfferer.Test/Services/When_requesting_loan_in_request_loan_service.cs
+++ backend/LoanOfferer.Test/Services/When_requesting_loan_in_request_loan_service.cs
@@ -1,7 +1,102 @@
+using System.Threading.Tasks;
+using LoanOfferer.Domain.Entities;
+using LoanOfferer.Domain.Repositories;
+using LoanOfferer.Domain.Services;
+using LoanOfferer.Domain.ValueObjects;
+using LoanOfferer.Services;
+using Moq;
+using Xunit;
+
 namespace LoanOfferer.Test.Services
 {
     public class When_requesting_loan_in_request_loan_service
     {
-        
+        [Fact]
+        public async Task It_should_get_loan_offer_from_repository_using_offer_id()
+        {
+            // Given
+            const int requestedAmount = 5000;
+            var offerEntityIdentity = EntityIdentity.New;
+            var repositoryMock = CreateLoanOfferRepositoryMock(offerEntityIdentity);
+            var service = CreateRequestLoanService(repositoryMock);
+
+            // When
+            await service.RequestLoan(offerEntityIdentity.ToString(), requestedAmount);
+
+            // Then
+            repositoryMock.Verify(m => m.GetAsync(It.Is<EntityIdentity>(id => id.Value == offerEntityIdentity.Value)), Times.Once);
+        }
+
+        [Fact]
+        public async Task It_should_call_set_requested_loan_amount_with_given_loan_amount()
+        {
+            // Given
+            const int requestedAmountPrimitive = 5000;
+            var requestedLoanAmount = new LoanAmount(requestedAmountPrimitive);
+            var offerEntityIdentity = EntityIdentity.New;
+            var loanOffer = new Mock<ILoanOffer>();
+            var repositoryMock = CreateLoanOfferRepositoryMock(offerEntityIdentity, loanOffer);
+            var service = CreateRequestLoanService(repositoryMock);
+
+            // When
+            await service.RequestLoan(offerEntityIdentity.ToString(), requestedLoanAmount.Value);
+
+            // Then
+            loanOffer.Verify(m => m.SetRequestedLoanAmount(It.Is<LoanAmount>(amount => amount.Value == requestedLoanAmount.Value)), Times.Once);
+        }
+
+        [Fact]
+        public async Task It_should_call_update_in_repository_using_the_same_loan_offer()
+        {
+            // Given
+            const int requestedAmountPrimitive = 5000;
+            var requestedLoanAmount = new LoanAmount(requestedAmountPrimitive);
+            var offerEntityIdentity = EntityIdentity.New;
+            var loanOffer = new Mock<ILoanOffer>();
+            var repositoryMock = CreateLoanOfferRepositoryMock(offerEntityIdentity, loanOffer);
+            var service = CreateRequestLoanService(repositoryMock);
+
+            // When
+            await service.RequestLoan(offerEntityIdentity.ToString(), requestedLoanAmount.Value);
+
+            // Then
+            repositoryMock.Verify(m => m.UpdateAsync(loanOffer.Object), Times.Once);
+        }
+
+        [Fact]
+        public async Task It_should_call_email_notification_service_with_parameters_from_loan_offer()
+        {
+            // Given
+            const int requestedAmountPrimitive = 5000;
+            var requestedLoanAmount = new LoanAmount(requestedAmountPrimitive);
+            var offerEntityIdentity = EntityIdentity.New;
+            var loanOffer = new Mock<ILoanOffer>();
+            var repositoryMock = CreateLoanOfferRepositoryMock(offerEntityIdentity, loanOffer);
+            var emailNotificationServiceMock = new Mock<IEmailNotificationService>();
+            var service = CreateRequestLoanService(repositoryMock, emailNotificationServiceMock);
+
+            // When
+            await service.RequestLoan(offerEntityIdentity.ToString(), requestedLoanAmount.Value);
+
+            // Then
+            emailNotificationServiceMock.Verify(m => m.SendLoanRequestedMessage(loanOffer.Object.EmailAddress, loanOffer.Object.RequestedLoanAmount), Times.Once);
+        }
+
+        private static Mock<ILoanOfferRepository> CreateLoanOfferRepositoryMock(EntityIdentity offerEntityIdentity, IMock<ILoanOffer> loanOfferMock = null)
+        {
+            var mock = new Mock<ILoanOfferRepository>();
+            mock.Setup(m => m.GetAsync(It.Is<EntityIdentity>(id => id.Value == offerEntityIdentity.Value)))
+                .ReturnsAsync(loanOfferMock?.Object ?? new Mock<ILoanOffer>().Object);
+            return mock;
+        }
+
+        private static RequestLoanService CreateRequestLoanService(
+            IMock<ILoanOfferRepository> loanOfferRepositoryMock = null,
+            IMock<IEmailNotificationService> emailNotificationService = null
+        )
+            => new RequestLoanService(
+                loanOfferRepositoryMock?.Object ?? new Mock<ILoanOfferRepository>().Object,
+                emailNotificationService?.Object ?? new Mock<IEmailNotificationService>().Object
+            );
     }
 }
