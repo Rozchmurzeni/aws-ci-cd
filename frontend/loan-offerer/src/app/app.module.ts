54006c9e33413125da26f5271b01fed346975822
Wojciech Dąbrowski <wojciech.dabrowski@northmill.se>
2019-08-18 13:46:23 +0200

    Add error handling

--- frontend/loan-offerer/src/app/app.module.ts
+++ frontend/loan-offerer/src/app/app.module.ts
@@ -2,17 +2,24 @@ import { LoanOfferService } from "./loan-offer/loan-offer.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { LoanOfferComponent } from "./loan-offer/loan-offer.component";
 import { DomainValidators } from "./domain-validators";
 import { ConfigProvider } from "./config-provider";
+import { HttpErrorInterceptor } from "./http-error.interceptor";
+import { ProcessFailedComponent } from './loan-offer/process-failed/process-failed.component';
 
 @NgModule({
-  declarations: [AppComponent, LoanOfferComponent],
+  declarations: [AppComponent, LoanOfferComponent, ProcessFailedComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-  providers: [LoanOfferService, DomainValidators, ConfigProvider],
+  providers: [
+    LoanOfferService,
+    DomainValidators,
+    ConfigProvider,
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
